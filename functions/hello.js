a030630f6b96179680abb0b3d92541999c61d3b8
Nishant Baser <nishant.baser@intelliatech.com>
2024-01-09 11:50:08 +0530

    added serverlessresponse serverless function

--- functions/hello.js
+++ functions/hello.js
@@ -3,7 +3,7 @@
 exports.handler = async (event, context) => {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello, World!" }),
+      body: JSON.stringify({ message: "Hello, World!",serverlessresponse:"Response of serverless function" }),
     };
   };
   
